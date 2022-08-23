8d75586cf74545c14844f0c67c924a20121cfb62
nikolapivac <npivac00@fesb.hr>
2022-08-23 09:50:36 +0200

    Made message board

---
+++ routes/index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+
+const messages = [
+  {
+    title: 'Its so hot',
+    description:
+      'I need winter right now',
+    username: 'Armando',
+    date: new Date()
+  },
+  {
+    title: 'Lorde',
+    description:
+      'Lorde is my favorite artist',
+    username: 'Laurie',
+    date: new Date()
+  }
+];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Mini Message Board', messages: messages });
+});
+
+router.post('/new', function(req, res, next) {
+  const newMessage = {
+    title: req.body.title,
+    description: req.body.description,
+    username: req.body.username,
+    date: new Date()
+  };
+  messages.unshift(newMessage);
+  res.redirect('/');
+});
+
+module.exports = router;
